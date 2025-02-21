8065b46fe9f3197eb53919f013808bf92ce43cfb
deeshnna <deeshnnasreyas@gmail.com>
2025-02-21 16:46:20 +0530

    router issue fixed

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
-import Router from "./routes/router";
+import Router from "./routes/Router";
 import { store } from "./redux/store.js";
 import { DarkModeProvider } from "./context/DarkModeContext";
 import "./index.css";
